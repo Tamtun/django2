c8707b2d83ff74a26301d90844d09d24e910bc6d
Tamtun <inteslux@yandex.ru>
2025-08-10 19:41:11 +0700

    Homework 4

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import IndexView, HomeView, ProductDetailView, ContactsView
 
 urlpatterns = [
-    #path("", views.index, name="catalog-index"),
-    path('', views.home, name='home'),
-    path('products/<int:pk>/', views.product_detail, name='product_detail'),
-    path('contacts/', views.contacts, name='contacts')
+    path('', HomeView.as_view(), name='home'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('index/', IndexView.as_view(), name='catalog-index'),
+    path('blogs/', include('blog.urls')),
 ]
-
