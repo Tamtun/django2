f644c0d55a809c2a8b73b87a218b308d2a122a84
Tamtun <inteslux@yandex.ru>
2025-08-20 02:20:49 +0700

    Homework 5

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
-from .views import IndexView, HomeView, ProductDetailView, ContactsView
+from django.urls import path, include
+from .views import (
+    IndexView, HomeView, ProductDetailView, ContactsView,
+    ProductCreateView, ProductUpdateView, ProductDeleteView
+)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('index/', IndexView.as_view(), name='catalog-index'),
     path('blogs/', include('blog.urls')),
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,13 @@
 from django.views import View
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import (
+    ListView, DetailView, TemplateView,
+    CreateView, UpdateView, DeleteView
+)
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy, reverse
 from .models import Product
+from .forms import ProductForm
 
 class IndexView(View):
     def get(self, request):
@@ -20,3 +25,23 @@ class ProductDetailView(DetailView):
 
 class ContactsView(TemplateView):
     template_name = 'contacts.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+    success_url = reverse_lazy('home')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+
+    def get_success_url(self):
+        return reverse('product-detail', kwargs={'pk': self.object.pk})
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product_confirm_delete.html'
+    success_url = reverse_lazy('home')
