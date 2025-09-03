d6b5499e6da8e46395b3f711e99dc4e8a4be78a5
Tamtun <inteslux@yandex.ru>
2025-09-03 14:04:57 +0700

    fix

--- config/settings.py
+++ config/settings.py
@@ -155,10 +155,11 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 LOGIN_URL = 'users/login/'
 
 CACHE_ENABLED = True
+
 if CACHE_ENABLED:
     CACHES = {
         "default": {
             "BACKEND": "django.core.cache.backends.redis.RedisCache",
-            "LOCATION": "redis://localhost:6379"
+            "LOCATION": os.getenv("REDIS_URL"),
         }
     }
